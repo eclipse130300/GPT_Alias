a47959b076dbcc340d15f5794f2ba5393c8a83e0
eclipse130300 <eclipse130300@yandex.ru>
2023-09-23 01:29:32 +0300

    2 more components added

---
+++ Assets/_MAIN/Scripts/UI/Components/CurrentRoundText.cs
@@ -0,0 +1,20 @@
+﻿using AliasGPT;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class CurrentRoundText : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _text;
+
+    [Inject]
+    private GameContext _gameContext;
+
+    private void Awake() => 
+        _gameContext.CurrentRound.Subscribe(OnLastRoundChanged).AddTo(this);
+
+    private void OnLastRoundChanged(uint currentRound) => 
+        _text.text = $"{currentRound}/{_gameContext.MaxRounds}";
+}
---
+++ Assets/_MAIN/Scripts/UI/Components/ThemeText.cs
@@ -0,0 +1,20 @@
+﻿using AliasGPT;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+public class ThemeText : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _text;
+
+    [Inject]
+    private GameContext _gameContext;
+
+    private void Awake() => 
+        _gameContext.CommonThemeName.Subscribe(OnLastRoundChanged).AddTo(this);
+
+    private void OnLastRoundChanged(string themeName) => 
+        _text.text = themeName;
+}
